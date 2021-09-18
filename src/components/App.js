c808983c38d3c402e0ba69d23dc930728908f1c6
brian4548 <brianr4548@gmail.com>
2021-09-18 16:32:16 -0500

    added navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 
 import StreamShow from "./streams/StreamShow";
 import StreamEdit from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
+import Header from "./Header";
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
+        
       <BrowserRouter>
         <div>
-          <Route patch="/" exact component={StreamList} />
-          <Route patch="/streams/new" exact component={StreamCreate} />
-          <Route patch="/streams/edit" exact component={StreamEdit} />
-          <Route patch="/streams/delete" exact component={StreamDelete} />
-          <Route patch="/streams/show" exact component={StreamShow} />
+        <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
