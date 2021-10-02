cd0b75c333495d92f06d342ea3f916d9f74c6eff
brian4548 <brianr4548@gmail.com>
2021-10-01 21:11:12 -0500

    added bulk restful action creators for later

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,55 @@
-import streams from '../apis/streams'
-import StreamCreate from '../components/streams/StreamCreate'
-import { SIGN_IN } from "./types"
-import { SIGN_OUT } from "./types"
-import { CREATE_STREAM } from './types'
+import { formValues } from "redux-form";
+import streams from "../apis/streams";
+import StreamCreate from "../components/streams/StreamCreate";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    }
-}
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
 
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    }
-}
+  return {
+    type: SIGN_OUT,
+  };
+};
 
 export const createStream = (formValues) => async (dispatch) => {
-    const response = await streams.post('/streams', formValues)
+  const response = await streams.post("/streams", formValues);
 
-    dispatch({ type: CREATE_STREAM, payload: response.data})
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
 
-}
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  const response = await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
