b29c0b0b729975eff5617fdb1ee7f526adc0b74d
brian4548 <brianr4548@gmail.com>
2021-10-17 22:03:15 -0500

    refactored the form component to be resuable. Used the form component form create/edit. Added redirects in the action creators. changed the editStream request to PATCH.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,80 +1,25 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+
 import { Link } from "react-router-dom";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 import { createStream } from "../../actions";
 
+import StreamForm from "./StreamForm";
+
 class StreamCreate extends React.Component {
-  renderError = ({ error, touched }) => {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
-  renderInput = (formProps) => {
-    
-
-    const fieldError = `field ${
-      formProps.meta.error && formProps.meta.touched ? "error" : null
-    }`;
-    return (
-      <div className={fieldError}>
-        <label>{formProps.label}</label>
-        <input
-          // onChange={formProps.input.onChange}
-          // value={formProps.input.value}
-          // or shorter syntax {...formProps.input} takes the formProps input property (that is an  objext that contains the value prop and onchange) and add them are properties to the input element
-          {...formProps.input}
-          autoComplete="off"
-        />
-        {this.renderError(formProps.meta)}
-      </div>
-    );
-  };
-
   onSubmit = (formValues) => {
-    this.props.createStream(formValues)
+    this.props.createStream(formValues);
   };
   render() {
-    
     return (
-      <form
-        className="ui form error"
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        // handleSubmit comes from redux-form
-      >
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        {/* Field doesnt know what a 'label' is. So because of this, it passes it to the component property and assigns it accordingly. this is how you can customize redux forms */}
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>
+          Create a Stream
+        </h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = "you must enter a title";
-  }
-
-  if (!formValues.description) {
-    errors.description = "you must enter a description";
-  }
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: "streamCreate",
-  validate: validate,
-})(StreamCreate);
-
-export default connect(null, {createStream})(formWrapped)
+export default connect(null, { createStream })(StreamCreate);
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,17 @@
+import _ from 'lodash'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
+
 
 class StreamEdit extends React.Component{
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues)
+    
+    }
 
     componentDidMount = () => {
         this.props.fetchStream(this.props.match.params.id)
@@ -17,7 +25,11 @@ class StreamEdit extends React.Component{
         } 
         return(
             <div>
-                StreamEdit
+                <h3>Edit a Stream</h3>
+                <StreamForm 
+                onSubmit={this.onSubmit} 
+                initialValues={_.pick(this.props.stream, 'title', 'description')}
+                />
             </div>
         )
     }
@@ -29,4 +41,4 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps, {fetchStream})(StreamEdit)
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit)
