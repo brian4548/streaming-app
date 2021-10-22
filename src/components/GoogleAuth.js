82da1ad54b7a03fdb746e068a334c3e8c87b8bcb
brian4548 <brianr4548@gmail.com>
2021-10-21 23:43:45 -0500

    deleted the if statement on renderbutton that makes it null if isSignedIn is null

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -38,9 +38,11 @@ class GoogleAuth extends React.Component {
   }
 
   renderAuthButton() {
-    if (this.props.isSignedIn === null) {
-      return null;
-    } else if (this.props.isSignedIn === true) {
+    // if (this.props.isSignedIn === null) {
+    //   return null;
+    // } else 
+    
+    if (this.props.isSignedIn === true) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
